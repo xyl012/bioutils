c1f6e59c5cf0384563d95d510bc0f2ca3e93498e
otsukaresamaja <christophersugai@gmail.com>
2020-12-06 17:33:24 -1000

    test

--- src/charsets/quality.rs
+++ src/charsets/quality.rs
@@ -1,6 +1,6 @@
 // Copyright 2020 Christopher Sugai
 
-//! Quality character (sub-)sets including Phred33, Phred64, and Solexa/Illumina 1.0 (for compatibility)
+//! Quality character (sub-)sets including Phred33, Phred64, and Solexa/Illumina 1.0 (for compatibility). Provided as u8 and str arrays.
 
 // Phred33 charset: ASCII 33-126
 pub const PHRED33_CHARSET_U8: [u8; 94] = [b'!', b'"', b'#', b'$', b'%', b'&', 0x0027, b'(', b')', b'*', b'+', b',', b'-', b'.', b'/', b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b':', b';', b'<', b'=', b'>', b'?', b'@', b'A', b'B', b'C', b'D', b'E', b'F', b'G', b'H', b'I', b'J', b'K', b'L', b'M', b'N', b'O', b'P', b'Q', b'R', b'S', b'T', b'U', b'V', b'W', b'X', b'Y', b'Z', 0x005B, 0x005C, 0x005D, b'^', b'_', b'`', b'a', b'b', b'c', b'd', b'e', b'f', b'g', b'h', b'i', b'j', b'k', b'l', b'm', b'n', b'o', b'p', b'q', b'r', b's', b't', b'u', b'v', b'w', b'x', b'y', b'z', b'{', b'|', b'}', b'~'];
--- src/utils/check.rs
+++ src/utils/check.rs
@@ -1,35 +1,57 @@
-// // Copyright 2020 Christopher Sugai
+// Copyright 2020 Christopher Sugai
 
-// //! Checks and classifications returning booleans for an input sequence and basic user input. For example, dna.is_homopolymer(90) will return boolean true if dna is comprised of >90% a single nucleotide. 
-// //! Useful external crates to use in combination with these functions: std ascii, especially is_ascii(), rust regex to check for punctuation
-// //! # Examples
-// //! ```
-// //! ```
+//! Checks and classifications returning booleans for an input sequence and basic user input. For example, dna.is_homopolymer(90) returns boolean true if dna is comprised of >90% a single nucleotide. 
+//! # Examples
+//! ```
+//! ```
 
+use crate::charsets::iupac::*;
+use crate::charsets::quality::*;
+use std::str::from_utf8;
 
-// trait CheckSequence <T: From<u8>> {
+trait CheckSequence<T: From<u8>> {
 
-//     // sequence checks whether valid IUPAC sequences
-//     fn is_iupac_nucleotide_sequence(&self) -> Self;
-//     fn is_iupac_amino_acid_sequence(&self) -> Self;
-//     fn is_basic_nucleotide_sequence(&self) -> Self;
-//     fn is_basic_amino_acid_sequence(&self) -> Self;
-//     fn has_gap(&self) -> Self;
-//     fn has_n(&self) -> Self;
-//     fn has_mixed_case(&self) -> Self;
-//     fn has_seq(&self) -> Self;
+    // // sequence checks
+    // fn has_gap(&self) -> bool;
+    // fn has_n(&self) -> bool;
+    // fn has_mixed_case(&self) -> bool;
+    // fn has_seq(&self) -> bool;
+    fn is_iupac_nucleotide(&self) -> bool;
+    // fn is_iupac_amino_acid(&self) -> bool;
+    // fn is_basic_nucleotide(&self) -> bool;
+    // fn is_basic_amino_acid(&self) -> bool;
+    // // quality checks whether valid sequences of phred33, phred64, or solexa quality characters
+    // fn is_phred33(&self) -> bool;
+    // fn is_phred64(&self) -> bool;
+    // fn is_solexa(&self) -> bool;
 
-//     // quality checks whether valid sequences of quality characters and filtering utilities
-//     fn is_quality_sequence(&self) -> Self; // the same as is_phred33_quality_sequence
-//     fn is_phred33_quality_sequence(&self) -> Self;
-//     fn is_phred64_quality_sequence(&self) -> Self;
-//     fn is_solexa_quality_sequence(&self) -> Self;
+    // // checks whther sequence is within user given bounds
+    // fn is_quality(&self) -> bool; 
 
-//     fn is_quality(&self) -> Self;
-//     fn is_quality(&self) -> Self;
+    // //Exact sequence classification (no distance difference)
+    // fn is_homopolymer(&self) -> bool;
+    // fn is_homopolymer_n(&self) -> bool;
+    // fn is_homopolymer_not_n(&self) -> bool;
+    // fn is_palindrome(&self) -> bool;
 
-//     //sequence classification
-//     fn is_homopolymer(&self) -> Self;
-//     fn is_palindrome(&self) -> Self;
-// }
+    // //sequence classification within user given Hamming distance
+    // fn is_inexact_homopolymer(&self) -> bool;
+    // fn is_inexact_palindrome(&self) -> bool;
+}
 
+impl<T> dyn CheckSequence<T> where T: From<u8> {
+    fn is_iupac_nucleotide(&self) -> bool {
+        if IUPAC_NUCLEOTIDE_U8.contains(self as u8) {true} else{false}
+        // if self.iter().any(|&IUPAC_NUCLwEOTIDE_U8| IUPAC_NUCLEOTIDE_U8==self) {
+        //     true
+        }
+    // fn is_iupac_amino_acid(&self) -> bool {
+    //     true
+    // }
+    // fn is_basic_nucleotide(&self) -> bool {
+    //     true
+    // }
+    // fn is_basic_amino_acid(&self) -> bool {
+    //     true
+    // }
+}

6fc04ab32481a1f77cbec43c6e2e2ce1d3ba893e
otsukaresamaja <christophersugai@gmail.com>
2020-12-09 19:09:26 -1000

    working

--- src/utils/check.rs
+++ src/utils/check.rs
@@ -3,23 +3,30 @@
 //! Checks and classifications returning booleans for an input sequence and basic user input. For example, dna.is_homopolymer(90) returns boolean true if dna is comprised of >90% a single nucleotide. 
 //! # Examples
 //! ```
+//! 
 //! ```
 
 use crate::charsets::iupac::*;
 use crate::charsets::quality::*;
-use std::str::from_utf8;
-use std::convert::From;
-trait CheckSequence<T: From<u8>> {
 
-    // // sequence checks
-    // fn has_gap(&self) -> bool;
-    // fn has_n(&self) -> bool;
+/// Trait for checking specific criteria for a [u8] of biological file origin. Types include sequence (nucleotide/amino acid) and quality (phred33/64/solexa, solexa being all printable ascii).
+/// Additional functionality for common checks including has_n, has_gap, is_homopolymer, is_palindrome, etc.
+pub trait Check<T> {
+
+    // Checks if [u8] is valid iupac, etc.
+    fn is_iupac(&self) -> bool;
+    fn is_iupac_nucleotide(&self) -> bool;
+    fn is_iupac_amino_acid(&self) -> bool;
+    fn is_basic_dna(&self) -> bool;
+    fn is_basic_rna(&self) -> bool;
+    fn is_basic_amino_acid(&self) -> bool;
+
+    // Checks for combining with validity checks
+    fn has_gap(&self) -> bool;
+    fn has_n(&self) -> bool;
     // fn has_mixed_case(&self) -> bool;
     // fn has_seq(&self) -> bool;
-    fn is_iupac_nucleotide(&self) -> bool;
-    // fn is_iupac_amino_acid(&self) -> bool;
-    // fn is_basic_nucleotide(&self) -> bool;
-    // fn is_basic_amino_acid(&self) -> bool;
+
     // // quality checks whether valid sequences of phred33, phred64, or solexa quality characters
     // fn is_phred33(&self) -> bool;
     // fn is_phred64(&self) -> bool;
@@ -39,21 +46,30 @@ trait CheckSequence<T: From<u8>> {
     // fn is_inexact_palindrome(&self) -> bool;
 }
 
+impl<T> Check<T> for [u8] where for<'a> &'a T: IntoIterator<Item = &'a T> {
 
-// impl<T> dyn CheckSequence<T> where T: From<u8> {
-//     fn is_iupac_nucleotide(&self) -> bool {
-//         let u: u8 = self;
-//         if IUPAC_NUCLEOTIDE_U8.contains(&u) {true} else{false}
-//         // if self.iter().any(|&IUPAC_NUCLEOTIDE_U8| IUPAC_NUCLEOTIDE_U8==self) {
-//         //     true
-//         }
-//     // fn is_iupac_amino_acid(&self) -> bool {
-//     //     true
-//     // }
-//     // fn is_basic_nucleotide(&self) -> bool {
-//     //     true
-//     // }
-//     // fn is_basic_amino_acid(&self) -> bool {
-//     //     true
-//     // }
-// }
+    fn is_iupac(&self) -> bool {
+        self.into_iter().all(|x| IUPAC_U8.contains(&x))
+    }
+
+    fn is_iupac_nucleotide(&self) -> bool {
+    self.into_iter().all(|x| IUPAC_NUCLEOTIDE_U8.contains(&x))
+    }
+ 
+    fn is_iupac_amino_acid(&self) -> bool {
+        self.into_iter().all(|x| IUPAC_AMINO_ACID_U8.contains(&x))
+    }
+
+    fn is_basic_dna(&self) -> bool {
+        self.into_iter().all(|x| BASIC_DNA_U8.contains(&x))
+    }
+
+    fn is_basic_rna(&self) -> bool {
+        self.into_iter().all(|x| BASIC_RNA_U8.contains(&x))
+    }
+
+    fn is_basic_amino_acid(&self) -> bool {
+        self.into_iter().all(|x| BASIC_AMINO_ACID_U8.contains(&x))
+    }
+
+}

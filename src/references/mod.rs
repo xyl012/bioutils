f9696151cce0db0dcc2a0c012e3ced907aa1f039
otsukaresamaja <christophersugai@gmail.com>
2021-03-27 18:03:23 -0500

    human

--- src/references/mod.rs
+++ src/references/mod.rs
@@ -1,7 +1,5 @@
 // Copyright 2021 Christopher Sugai
 
-//! Currently includes human NCBI gencode GRCh38 and UCSC hg38. Automatically downloads the latest version of user's choice.
+//! Currently includes human NCBI gencode GRCh38. Automatically downloads the latest version of user's choice.
 
-pub mod gencode;
-pub mod functions;
 pub mod ftp;

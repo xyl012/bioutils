d56156d74b6ec09e0ec15310b57115931afc3ecc
otsukaresamaja <christophersugai@gmail.com>
2021-05-10 20:09:44 -0700

    image example

--- examples/step5_make_visualizations.rs
+++ examples/step5_make_visualizations.rs
@@ -3,19 +3,18 @@
 
 
 use bioutils::files::image::color::*;
-use image::{ImageBuffer, Rgb};
 use image::RgbImage;
-const WIDTH: u32 = 100;
-const HEIGHT: u32 = 100;
-use std::convert::TryInto;
+const WIDTH: u32 = 50;
+const HEIGHT: u32 = 50;
 
+use bioutils::utils::new::random::random_dna;
 
 fn main() {
-    let mut img = RgbImage::new(HEIGHT, WIDTH);
-    let seq = b"CCCCAAAAAATTTTTGGGGNNNNN";
-    let y_pixel_coord=50;
-    println!("{:?}", seq);
-    for (x, &c) in seq.into_iter().enumerate() {
+    let mut img = RgbImage::new(HEIGHT, WIDTH); // Make a new image with height and width
+    let seq = b"CCCCAAAAAATTTTTGGGGNNNNN"; // Make a sequence
+    let y_pixel_coord=49; // Set our y coordinates to 49 for first example
+    println!("{:?}", seq); // Print our unsigned integers for reference
+    for (x, &c) in seq.into_iter().enumerate() { // For each unsigned integer in our sequence, color the pixel corresponding to the x coordinate and y coordinate, color by the unsigned integer present.
         match c {
             67 => img.put_pixel(x as u32, y_pixel_coord, RED_RGB), // 67 is b"A", set Adenosine color
             65 => img.put_pixel(x as u32, y_pixel_coord, GREEN_RGB), // 65 is b"C", set Cytosine color
@@ -25,8 +24,25 @@ fn main() {
             _ => img.put_pixel(x as u32, y_pixel_coord, BLACK_RGB), // 71 is b"G", set all other as black
         };
     }
-    // Same with random sequences for the whole file
-    
     // write it out to a file
-    img.save("output.png").unwrap();
+    img.save("output1.png").unwrap();
+
+    // Let's do the same thing with random sequences for the whole file
+    for y in 0..HEIGHT {
+        let rng = rand::thread_rng(); // Create a random number generator
+        let dna = random_dna(50,rng); // Get a random dna sequence of length 50
+        for (x, c) in dna.into_iter().enumerate() {
+            match c {
+                67 => img.put_pixel(x as u32, y, RED_RGB), // 67 is b"A", set Adenosine color
+                65 => img.put_pixel(x as u32, y, GREEN_RGB), // 65 is b"C", set Cytosine color
+                84 => img.put_pixel(x as u32, y, BLUE_RGB), // 84 is b"T", set Thymine color
+                71 => img.put_pixel(x as u32, y, CYAN_RGB), // 71 is b"G", set Guanine color
+                78 => img.put_pixel(x as u32, y, GRAY_RGB), // 110 is b"N", set Undetermined color
+                _ => img.put_pixel(x as u32, y, BLACK_RGB), // 71 is b"G", set all other as black
+            };
+        }
+    }
+
+    // write it out to a file
+    img.save("output2.png").unwrap();
 }

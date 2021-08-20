7f9f55e0f02c3f4d14b40a3afc1a2458e7bfeff8
otsukaresamaja <christophersugai@gmail.com>
2021-08-19 21:41:22 -0700

    new functions take a slice

--- src/utils/item/new.rs
+++ src/utils/item/new.rs
@@ -2,15 +2,20 @@ use super::*;
 
 pub trait RandomBioVec<T> {
     /// Create new random sequences with specified number of elements.
-    fn random_vec_with(nbases: &usize, charset: BioUtilsCharSet) -> Result<Vec<u8>>;
+    fn random_vec(nbases: &usize, charset: BioUtilsCharSet) -> Result<Vec<u8>>;
     /// Creates new u8 from the chosen characterset
-    fn random_u8_with(charset: BioUtilsCharSet) -> Result<u8>;
+    fn random_u8(charset: BioUtilsCharSet) -> Result<u8>;
+
+    /// Create new random sequences with specified number of elements.
+    fn random_vec_with(nbases: &usize, charset: &[u8]) -> Result<Vec<u8>>;
+    /// Creates new u8 from the chosen characterset
+    fn random_u8_with(charset: &[u8]) -> Result<u8>;
 }
 
 impl<T> RandomBioVec<T> for T
 {
     /// Create new random sequences with specified number of elements.
-    fn random_vec_with(nbases: &usize, charset: BioUtilsCharSet) -> Result<Vec<u8>> {
+    fn random_vec(nbases: &usize, charset: BioUtilsCharSet) -> Result<Vec<u8>> {
         let mut rng = rand::thread_rng();
         let mut vec = Vec::with_capacity(*nbases);
         for _base in 0..*nbases {
@@ -19,8 +24,22 @@ impl<T> RandomBioVec<T> for T
         vec.into_iter().collect::<Option<Vec<u8>>>().ok_or(bail!("Cannot create new random"))
     }
     /// Creates new u8 from the chosen characterset
-    fn random_u8_with(charset: BioUtilsCharSet) -> Result<u8> {
+    fn random_u8(charset: BioUtilsCharSet) -> Result<u8> {
         let mut rng = rand::thread_rng();
         charset.value().choose(&mut rng).copied().ok_or(bail!("Cannot create new random"))
+    }
+    /// Create new random sequences with specified number of elements.
+    fn random_vec_with(nbases: &usize, charset: &[u8]) -> Result<Vec<u8>> {
+        let mut rng = rand::thread_rng();
+        let mut vec = Vec::with_capacity(*nbases);
+        for _base in 0..*nbases {
+            vec.push(charset.choose(&mut rng).copied().to_owned())
+        };
+        vec.into_iter().collect::<Option<Vec<u8>>>().ok_or(bail!("Cannot create new random"))
+    }
+    /// Creates new u8 from the chosen characterset
+    fn random_u8_with(charset: &[u8]) -> Result<u8> {
+        let mut rng = rand::thread_rng();
+        charset.choose(&mut rng).copied().ok_or(bail!("Cannot create new random"))
     }
 }
